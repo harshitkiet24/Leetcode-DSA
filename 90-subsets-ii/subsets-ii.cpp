c678775ad6fa368bf40eae1982cb7bc4d2000050
Harshit Tyagi <tyagiharshit758@gmail.com>
2026-08-06 00:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (97.50%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void func(vector<int>&arr,vector<int>&ds,int idx,vector<vector<int>> &subset){
+        subset.push_back(ds);
+        if(idx == arr.size()){
+            return;
+        }
+        for(int i = idx;i < arr.size();i++){
+            if(i > idx && arr[i] == arr[i-1]){
+                continue;
+            }
+            ds.push_back(arr[i]);
+            func(arr,ds,i+1,subset);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> subset;
+        vector<int> ds;
+        func(nums,ds,0,subset);
+        return subset;
+    }
+};
