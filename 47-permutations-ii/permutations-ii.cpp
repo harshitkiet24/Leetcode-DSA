f97d47308069c9aaddf80d493d1227505610a743
Harshit Tyagi <tyagiharshit758@gmail.com>
2026-08-10 01:07:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (84.04%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    void func(vector<int> &nums,vector<int>&ds,vector<vector<int>> &ans, map<int,int> mp){
-        if(ds.size() == nums.size()){
+    void func(vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans,
+              map<int, int> &mp) {
+        if (ds.size() == nums.size()) {
             ans.push_back(ds);
             return;
         }
 
-
-        for(auto &it : mp){
+        for (auto& it : mp) {
             int val = it.first;
-            if(it.second > 0){
+            if (it.second > 0) {
                 it.second--;
                 ds.push_back(val);
-                func(nums,ds,ans,mp);
+                func(nums, ds, ans, mp);
                 it.second++;
                 ds.pop_back();
             }
@@ -21,11 +21,11 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<int> ds;
         vector<vector<int>> ans;
-        map <int,int> mp;
-        for(int i=0;i<nums.size();i++){
+        map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
             mp[nums[i]]++;
         }
-        func(nums,ds,ans,mp);
+        func(nums, ds, ans, mp);
         return ans;
     }
 };
